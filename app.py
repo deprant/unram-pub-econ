04a3ceb7fc61eb51326788bace327d791618fdac
ade paranata <61595530+deprant@users.noreply.github.com>
2026-06-10 07:55:28 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 import streamlit as st
 import pandas as pd
 import gspread
+import os
+print(os.path.exists("cbt-streanmlit-9a9fa5a7d941.json"))
 from datetime import datetime
 from google.oauth2.service_account import Credentials
 
